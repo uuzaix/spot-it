df1981265d586e84886d595678a587dc69fb3b79
uuzaix <larisa.dolgova@gmail.com>
2017-11-06 22:49:23 +0100

    Show common pic in selected cards

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,44 @@ let v = new Vue({
   el: "#app",
   template: `
     <div>
-        <div>
-            Spot-it
-        </div>
+      <div>
+        Spot-it
+      </div>
         <div v-for="card, idx in cards">
-             <div class="card"
-                  :class="{'card--selected': selected.includes(idx)}"
-                  v-on:click="itemClicked(idx)">
-                {{card.join(' ')}}
-             </div>
+          <div class="card"
+               :class="{'card--selected': selected.includes(idx)}"
+               v-on:click="itemClicked(idx)">
+            <span v-for="pic in card"
+                  :class="{'image--common': selected.includes(idx) && commonImage === pic}">
+              {{pic}}
+            </span>
+           </div>
         </div>
     </div>`,
   data: {
     cards: createDeck(7),
     selected: [],
   },
-    methods: {
-      itemClicked(id) {
-          if (this.selected.includes(id)) {
-              this.selected = this.selected.filter(card => card !== id)
-          } else {
-              if (this.selected.length < 2) {
-                this.selected = [...this.selected, id]
-              } else {
-                  this.selected = [id]
-              }
-          }
+  computed: {
+    commonImage() {
+      if (this.selected.length === 2) {
+        const [firstId, secondId] = this.selected
+        return this.cards[firstId]
+          .filter(pic => this.cards[secondId].includes(pic))[0]
       }
     }
+  },
+  methods: {
+    itemClicked(id) {
+      if (this.selected.includes(id)) {
+        this.selected = this.selected.filter(card => card !== id)
+      } else {
+        if (this.selected.length < 2) {
+          this.selected = [...this.selected, id]
+        } else {
+          this.selected = [id]
+        }
+      }
+    }
+  },
 });
