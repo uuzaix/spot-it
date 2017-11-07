637ce3b2526bd0a6cac6deb18f0cf13899f58520
uuzaix <larisa.dolgova@gmail.com>
2017-11-07 23:55:45 +0100

    Use ramda chain and map

--- src/math.ts
+++ src/math.ts
@@ -1,4 +1,4 @@
-import { zipObj, range } from 'ramda'
+import {zipObj, range, chain, map} from 'ramda'
 
 type Point = number | number[]
 type Image = number
@@ -6,68 +6,58 @@ type Card = Image[]
 type Deck = Card[]
 
 function ordinaryPoints(n: number): number[] {
-    const result = []
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            result.push([i, j])
-        }
-    }
-    return result
+  const createPair = i => map(j => [i, j], range(0, n))
+  return chain(createPair, range(0, n))
 }
 
 function pointsAtInfinity(n: number): number[] {
-    return Array.from(Array(n).keys()).concat([1000])
+  return Array.from(Array(n).keys()).concat([1000])
 }
 
 function ordinaryLine(m: number, b: number, n: number): Point[] {
-    return range(0, n).map(i => [i, (m * i + b) % n]).concat([m])
+  return map(i => [i, (m * i + b) % n], range(0, n)).concat([m])
 }
 
 function verticalLine(x: number, n: number): Point[] {
-    return range(0, n).map(i => [x, i]).concat([1000])
+  return map(i => [x, i], range(0, n)).concat([1000])
 }
 
 function lineAtInfinity(n: number): Point[] {
-    return pointsAtInfinity(n)
+  return pointsAtInfinity(n)
 }
 
 function allPoints(n: number): Point[] {
-    return ordinaryPoints(n).concat(pointsAtInfinity(n))
+  return ordinaryPoints(n).concat(pointsAtInfinity(n))
 }
 
 function pointToString(point: Point): string {
-    return point + ''
+  return point + ''
 }
 
 function allLines(n: number): Point[][] {
-    const infLines = lineAtInfinity(n)
-    const vertLines = range(0, n).map(i => verticalLine(i, n))
-    const ordLines = []
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            ordLines.push(ordinaryLine(i, j, n))
-        }
-    }
-    return ordLines.concat(vertLines, [infLines])
+  const infLines = lineAtInfinity(n)
+  const vertLines = map(i => verticalLine(i, n), range(0, n))
+  const ordLines = chain(i => map(j => ordinaryLine(i, j, n), range(0, n)), range(0, n))
+  return ordLines.concat(vertLines, [infLines])
 }
 
-function generatePics(n: number): Image[]{
-    return range(0, n)
+function generatePics(n: number): Image[] {
+  return range(0, n)
 }
 
-function mapPointsToPics(points: Point[], pics: Image[]): {[s: string]: Image} {
-    const pointsAsString = points.map(pointToString)
-    return zipObj(pointsAsString, pics)
+function mapPointsToPics(points: Point[], pics: Image[]): { [s: string]: Image } {
+  const pointsAsString = map(pointToString, points)
+  return zipObj(pointsAsString, pics)
 }
 
 export function createDeck(n: number): Deck {
-    const points = allPoints(n)
-    const lines = allLines(n)
-    const pics = generatePics(points.length)
-    const mappings = mapPointsToPics(points, pics)
-    return lines
-        .map(line => line
-            .map(point => mappings[point+ '']
-        )
+  const points = allPoints(n)
+  const lines = allLines(n)
+  const pics = generatePics(points.length)
+  const mappings = mapPointsToPics(points, pics)
+  return lines
+    .map(line => line
+      .map(point => mappings[point + '']
+      )
     )
 }
